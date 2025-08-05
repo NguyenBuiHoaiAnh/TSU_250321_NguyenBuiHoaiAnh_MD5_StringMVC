e8a09b00a5854e168985a0219dbc893e28d47a48
NguyenBuiHoaiAnh <nguyenhoai8a@gmail.com>
2025-08-05 16:20:57 +0700

    Delete SS5/BT1_SS5 directory

--- SS5/BT1_SS5/src/main/java/com/example/bt1_ss5/model/UserSession.java
+++
@@ -1,5 +0,0 @@
-package com.example.bt1_ss5.model;
-
-public class UserSession {
-    public static Customer customer;
-}
--- SS5/BT1_SS5/src/main/java/com/example/bt1_ss5/repo/customer/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.bt1_ss5.repo.customer;
-
-import com.example.bt1_ss5.model.Customer;
-
-public interface CustomerDAO {
-    // Login Check
-    Customer checkUser(String username, String password);
-
-}
