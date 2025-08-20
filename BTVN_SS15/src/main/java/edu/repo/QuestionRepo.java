3cdc714653ea978c4f85160525a5d5d49924064e
NguyenBuiHoaiAnh <nguyenhoai8a@gmail.com>
2025-08-20 21:26:39 +0700

    Delete BTVN_SS15 directory

--- BTVN_SS15/src/main/java/edu/repo/QuestionRepo.java
+++
@@ -1,9 +0,0 @@
-package edu.repo;
-
-import edu.model.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepo extends JpaRepository<Question,Long> {
-}
