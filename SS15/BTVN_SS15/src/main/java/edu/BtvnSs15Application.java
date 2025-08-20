ebff8bdfd83f481f90706dd1adfbeffbec88fa16
NguyenBuiHoaiAnh <nguyenhoai8a@gmail.com>
2025-08-20 21:28:03 +0700

    Add files via upload

---
+++ SS15/BTVN_SS15/src/main/java/edu/BtvnSs15Application.java
@@ -0,0 +1,13 @@
+package edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtvnSs15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtvnSs15Application.class, args);
+    }
+
+}
